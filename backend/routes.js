f3b56ddaeb59c2cc3643dcf7e1cac1a3506b46f3
AndreJSON <andre.v.json@gmail.com>
2018-09-20 22:42:51 +0200

    Improves readability of error checking.

--- backend/routes.js
+++ backend/routes.js
@@ -7,13 +7,13 @@ const Recipe = require('./recipe-model');
 module.exports = (app) => {
 	app.get('/api/recipes', (req,res) => {
 		Recipe.find({}, (err, docs) => {
-			if(err === null) {
-				res.json({recipes: docs});
-				res.end();
-			} else {
+			if(err) {
 				console.log(err);
 				res.statusCode = 500;
 				res.statusMessage = "Failed to get recipes.";
+				res.end();
+			} else {
+				res.json({recipes: docs});
 				res.end();
 			}
 		});
@@ -22,13 +22,13 @@ module.exports = (app) => {
 	app.post('/api/recipe', (req,res) => {
 		let recipe = new Recipe({...req.body, created: new Date(), visible: true});
 		recipe.save((err) => {
-			if(err === null) {
-				res.json(recipe);
-				res.end();
-			} else {
+			if(err) {
 				console.log(err);
 				res.statusCode = 500;
 				res.statusMessage = "Failed to add recipe.";
+				res.end();
+			} else {
+				res.json(recipe);
 				res.end();
 			}
 		});
